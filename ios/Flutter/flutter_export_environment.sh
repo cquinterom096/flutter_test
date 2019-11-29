bc65538d834906f893c44ff7884cff70db7794e2
cquinterom096 <cquinterom096@outlook.com>
2019-11-29 17:58:35 -0500

    update pubspec

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/cquinterom096/Documents/workspace/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/cquinterom096/Documents/Dart/Flutter/onboarding_postman_test"
+export "FLUTTER_TARGET=/Users/cquinterom096/Documents/Dart/Flutter/onboarding_postman_test/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/cquinterom096/Documents/workspace/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
